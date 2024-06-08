791eb666fbc72a6c85358cda8de777fe562d7faf
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-08 12:56:33 -0400

    updated humidity.py

--- server/src/dht22/humidity.py
+++ server/src/dht22/humidity.py
@@ -1,15 +1,16 @@
-
+import time
 import adafruit_dht
 import board
 
 dht_device = adafruit_dht.DHT22(board.D17)
 
 try:
-        temperature_c = dht_device.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
+    temperature_c = dht_device.temperature
+    temperature_f = temperature_c * (9 / 5) + 32
 
-        humidity = dht_device.humidity
+    humidity = dht_device.humidity
 
-        print("Temp:{:.1f} C / {:.1f} F    Humidity: {}%".format(temperature_c, temperature_f, humidity))
-    except RuntimeError as err:
-        print(err.args[0])
+    print("Temp:{:.1f} C / {:.1f} F    Humidity: {}%".format(temperature_c, temperature_f, humidity))
+    
+except RuntimeError as err:
+    print(err.args[0])
