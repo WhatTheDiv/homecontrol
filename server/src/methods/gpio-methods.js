617f855df95b155113375c79dd0e9a6fe53711b9
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-08 15:16:56 -0400

    update

--- server/src/methods/gpio-methods.js
+++ server/src/methods/gpio-methods.js
@@ -30,33 +30,6 @@ const getIndoorTempReading = async () => {
     }
   }
 
-  // controller.abort()
-
-
-
-
-
-  // return new Promise((res, rej) => {
-  //   exec('cd src/dht22 && python3 humidity.py', {signal}, (error, stdout, stderr) => {
-  //     if (error) {
-  //       console.log(`error: ${error.message}`);
-  //       res({ temp: 99, humidity: 99 })
-  //     }
-  //     if (stderr) {
-  //       console.log(`stderr: ${stderr}`);
-  //       res({ temp: 99, humidity: 99 })
-  //     }
-  //     else {
-  //       console.log(`stdout: ${stdout}`);
-  //       const temp = stdout.slice(stdout.indexOf("Temp:" + 5), stdout.indexOf('F'))
-  //       const humidity = stdout.slice(stdout.indexOf('Humidity:' + 9), stdout.indexOf('%'))
-  //       console.log({
-  //         temp, humidity
-  //       })
-  //       res({ temp, humidity })
-  //     }
-  //   })
-  // })
 }
 
 module.exports = { getIndoorTempReading }
