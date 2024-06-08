3729938b277f30b44d54235c5298c43e0f6b4641
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-08 14:36:33 -0400

    update

--- server/src/methods/gpio-methods.js
+++ server/src/methods/gpio-methods.js
@@ -1,27 +1,44 @@
-const { exec } = require('child_process')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const controller = new AbortController()
+const { signal } = controller
 
 const getIndoorTempReading = async () => {
-  return new Promise((res, rej) => {
-    exec('cd src/dht22 && python3 humidity.py', (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        res({ temp: 99, humidity: 99 })
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        res({ temp: 99, humidity: 99 })
-      }
-      else {
-        console.log(`stdout: ${stdout}`);
-        const temp = stdout.slice(stdout.indexOf("Temp:" + 5), stdout.indexOf('F'))
-        const humidity = stdout.slice(stdout.indexOf('Humidity:' + 9), stdout.indexOf('%'))
-        console.log({
-          temp, humidity
-        })
-        res({ temp, humidity })
-      }
-    })
-  })
+  try {
+    const { stdout, stderr } = await exec('cd src/dht22 && python3 humidity.py', { signal })
+
+    console.log('stdout: ', stdout)
+    console.log('stderr: ', stderr)
+
+  } catch (e) {
+    console.log('failed to contact sensor')
+  }
+
+
+
+
+
+  // return new Promise((res, rej) => {
+  //   exec('cd src/dht22 && python3 humidity.py', {signal}, (error, stdout, stderr) => {
+  //     if (error) {
+  //       console.log(`error: ${error.message}`);
+  //       res({ temp: 99, humidity: 99 })
+  //     }
+  //     if (stderr) {
+  //       console.log(`stderr: ${stderr}`);
+  //       res({ temp: 99, humidity: 99 })
+  //     }
+  //     else {
+  //       console.log(`stdout: ${stdout}`);
+  //       const temp = stdout.slice(stdout.indexOf("Temp:" + 5), stdout.indexOf('F'))
+  //       const humidity = stdout.slice(stdout.indexOf('Humidity:' + 9), stdout.indexOf('%'))
+  //       console.log({
+  //         temp, humidity
+  //       })
+  //       res({ temp, humidity })
+  //     }
+  //   })
+  // })
 }
 
 module.exports = { getIndoorTempReading }
