9115b2933c10c08bc6d2b93223adf1054613033c
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-08 12:55:11 -0400

    updated humidity.py

--- server/src/dht22/humidity.py
+++ server/src/dht22/humidity.py
@@ -1,11 +1,10 @@
-import time
+
 import adafruit_dht
 import board
 
 dht_device = adafruit_dht.DHT22(board.D17)
 
-while True:
-    try:
+try:
         temperature_c = dht_device.temperature
         temperature_f = temperature_c * (9 / 5) + 32
 
@@ -14,5 +13,3 @@ while True:
         print("Temp:{:.1f} C / {:.1f} F    Humidity: {}%".format(temperature_c, temperature_f, humidity))
     except RuntimeError as err:
         print(err.args[0])
-
-    time.sleep(2.0)
