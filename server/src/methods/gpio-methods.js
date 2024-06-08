a6037e5bb9b595b267d954d7f11e05858a066a76
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-08 13:30:11 -0400

    update

--- server/src/methods/gpio-methods.js
+++ server/src/methods/gpio-methods.js
@@ -1,7 +1,30 @@
 const { spawn } = require('child_process')
 
 const getIndoorTempReading = () => {
+  const runPythonScript = (scriptPath, args) => {
+    const pyProg = spawn('python', [scriptPath].concat(args))
+
+    let data = ''
+    pyProg.stdout.on('data', (stdout) => {
+      data += stdout.toString()
+    })
+
+    pyProg.stderr.on('data', (stderr) => {
+      console.log('stderr: ', stderr)
+    })
+
+    pyProg.on('close', (code) => {
+      console.log('child process exited with code', code)
+      console.log(data)
+    })
+
+    runPythonScript('../dht22/humidity.py', [])
+  }
   console.log('checkpoint')
+
+
+
+
 }
 
 module.exports = { getIndoorTempReading }
