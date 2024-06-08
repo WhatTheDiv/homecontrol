135f1b10d756b937de0f82c0cb23d29d343de53e
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-08 15:55:48 -0400

    update

--- server/src/methods/gpio-methods.js
+++ server/src/methods/gpio-methods.js
@@ -40,7 +40,19 @@ const getIndoorTempReading = async () => {
     humidity: 99
   }
   try {
-    const childProcess = await exec('cd src/dht22 && python3 humidity.py')
+    const childProcess = await exec('cd src/dht22 && python3 humidity.py', (error, stdout, stderr) => {
+
+      if (error) throw new Error('There was an error in exec: ' + error)
+      if (stderr) throw new Error('stderr: ' + stderr)
+      if (stdout.indexOf('Temp:') === -1) throw new Error("Sensor failed: " + stdout)
+
+      const temp = Number(stdout.slice(stdout.indexOf("Temp:") + 5, stdout.indexOf('F')))
+      const humidity = Number(stdout.slice(stdout.indexOf('Humidity:') + 9, stdout.indexOf('%')))
+
+      returnObj.temp = temp
+      returnObj.humidity = humidity
+
+    })
 
     /*
     , (error, stdout, stderr) => {
