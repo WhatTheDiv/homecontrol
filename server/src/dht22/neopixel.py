8be4bafb97fee9ce5835f66cf47c91bb6b846919
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-11 20:19:48 -0400

    python led

--- server/src/dht22/neopixel.py
+++ server/src/dht22/neopixel.py
@@ -1,5 +1,8 @@
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 12)
-pixels[0] = (10, 0, 0)
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 12, auto_write=False)
+pixels[0] = (10, 0, 0, 10)
+pixels[0] = (0, 10, 0, 10)
+
+pixels.show()
