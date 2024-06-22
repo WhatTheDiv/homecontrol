37f85198f1f25f8c8e5bacde48da2c397f29cfb1
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-22 14:25:26 -0400

    fixing python

--- server/src/methods/gpio-methods.js
+++ server/src/methods/gpio-methods.js
@@ -10,7 +10,7 @@ const getIndoorTempReading = async () => {
     }
 
     try {
-      const childProcess = await exec('cd src/python && .venv/bin/python humidity.py', (error, stdout, stderr) => {
+      const childProcess = await exec('src/python/.venv/bin/python3 src/python/humidity.py', (error, stdout, stderr) => {
         if (error) {
           console.error('Error in process, server temp reading: ', stderr)
           throw new Error("Error in process, server temp reading: " + error)
