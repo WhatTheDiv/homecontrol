8e0b0d7eb3352c94729c57e6c95fa697f3620d58
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-21 22:14:05 -0400

    added python test code

--- server/src/dht22/test.py
+++ server/src/dht22/test.py
@@ -1,21 +1,44 @@
-# import gpiod
-# import time
-
-# RELAY_1 = 19
-
-# chip = gpiod.Chip('gpiochip4')
-# relay_line = chip.get_line(RELAY_1)
-
-# relay_line.request(consumer="RELAY", type=gpiod.LINE_REQ_DIR_OUT)
-
 import gpiod
+import time
 
-with gpiod.Chip("/dev/gpiochip4") as chip:
-    info = chip.get_info()
-    print(" ")
-    print("---------- Start ---------- ")
-    print(" ")
-    print(f"{info.name} [{info.label}] ({info.num_lines} lines)")
+RELAY_1 = 19
 
-    print(" ")
-    print("----------- End ----------- ")
+chip = gpiod.Chip('gpiochip4')
+relay_line = chip.get_line(RELAY_1)
+
+relay_line.request(consumer="RELAY_TEST", type=gpiod.LINE_REQ_DIR_OUT)
+
+try:
+  relay_line.set_value(1)
+  time.sleep(1)
+  relay_line.set_value(0)
+  time.sleep(1)
+finally: 
+  relay_line.release()
+
+# import time
+# from gpiod.line import Direction, Value
+
+# LINE = 5
+
+# with gpiod.request_lines(
+#     "/dev/gpiochip4",
+#     consumer="blink-example",
+#     config={
+#         LINE: gpiod.LineSettings(
+#             direction=Direction.OUTPUT, output_value=Value.ACTIVE
+#         )
+#     },
+# ) as request:
+#     while True:
+#         request.set_value(LINE, Value.ACTIVE)
+#         time.sleep(1)
+#         request.set_value(LINE, Value.INACTIVE)
+#         time.sleep(1)
+
+
+
+
+
+
+
