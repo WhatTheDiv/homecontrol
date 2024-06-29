3666375bd141c02213b4fe37f5ee4a6f22311de8
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-29 13:15:52 -0400

    fixing python env

--- server/src/methods/gpio-methods.js
+++ server/src/methods/gpio-methods.js
@@ -12,27 +12,27 @@ const getIndoorTempReading = async () => {
     try {
       const childProcess = await exec('cd src/python && env/bin/python3 scripts/humidity.py', (error, stdout, stderr) => {
 
-        console.log({ error, stderr, stdout })
-
         if (error) {
           console.error('Error in process, server temp reading: ', stderr)
           throw new Error("Error in process, server temp reading: " + error)
         }
-        if (stderr) {
+        else if (stderr) {
           console.error('Temperature read created error: ', stderr)
           throw new Error("Sensor error: " + stdout)
         }
-        if (stdout.indexOf('Temp:') === -1) {
+        else if (stdout.indexOf('Temp:') === -1) {
           console.error('Bad temperature response from server: ', stdout)
           res(returnObj)
         }
+        else {
+          const temp = Number(stdout.slice(stdout.indexOf("Temp:") + 5, stdout.indexOf('F')))
+          const humidity = Number(stdout.slice(stdout.indexOf('Humidity:') + 9, stdout.indexOf('%')))
 
-        const temp = Number(stdout.slice(stdout.indexOf("Temp:") + 5, stdout.indexOf('F')))
-        const humidity = Number(stdout.slice(stdout.indexOf('Humidity:') + 9, stdout.indexOf('%')))
+          console.log('Got temp and humidity', { temp, humidity })
+          returnObj.temp = temp
+          returnObj.humidity = humidity
+        }
 
-        console.log('Got temp and humidity', { temp, humidity })
-        returnObj.temp = temp
-        returnObj.humidity = humidity
 
       })
 
