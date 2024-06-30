6a70257972f33a145025026942ec4acc46906960
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-29 22:39:54 -0400

    update

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -3,7 +3,7 @@ from gpiozero import LED
 import time
 import sys
 
-def turnZoneOn(zone):
+def turnZoneOn(zone, newState):
   currState = zone_one_L.is_lit
   print("Zone ",zone,"currently lit? ",currState)
   
@@ -12,25 +12,23 @@ print(" ")
 print("-------   Starting Script   -------")
 print(" ")
 
-try:
-    n = len(sys.argv)
-    print("arguments: ",n," - ", sys.argv)
+if len(sys.argv) <= 2:
+      raise Exception(" Not given zone and state")
 
-    if n <= 2:
-        raise Exception(" Not given zone and state")
+zone = sys.argv[1]
+state = sys.argv[2]
 
-    zone = sys.argv[1]
-    state = sys.argv[2]
+zone_one_L = LED(pin=18, initial_value=False)
+zone_one_R = LED(pin=19, initial_value=False)
 
-    zone_one_L = LED(pin=18, initial_value=False)
-    zone_one_R = LED(pin=19, initial_value=False)
+zone_two_L = LED(pin=20, initial_value=True)
+zone_two_R = LED(pin=21, initial_value=True)
 
-    zone_two_L = LED(pin=20, initial_value=False)
-    zone_two_R = LED(pin=21, initial_value=False)
-
+try: 
     time.sleep(.25)
 
     print("Toggling relay, Zone", zone, "-",state)
+    turnZoneOn(zone, state)
     
     
   
