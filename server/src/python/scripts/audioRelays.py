89630dd93098bcb17a2b31dcf052cff91330682d
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-06-29 22:29:35 -0400

    update

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -3,44 +3,30 @@ from gpiozero import LED
 import time
 import sys
 
-relay_one = LED(19)
-relay_two = LED(20)
+def turnZoneOn(zone):
+  currState = zone_one_L.is_lit
+  print("Zone ",zone,"currently lit? ",currState)
+  
+
+print(" ")
+print("-------   Starting Script   -------")
+print(" ")
 
 zone = sys.argv[1]
 state = sys.argv[2]
 
-print(" ")
-print("-------   Starting Script   -------")
+zone_one_L = LED(pin=18, initial_value=False)
+zone_one_R = LED(pin=19, initial_value=False)
 
-print(" ")
-print("Toggling relay, Zone", zone, "-",state)
+zone_two_L = LED(pin=20, initial_value=False)
+zone_two_R = LED(pin=21, initial_value=False)
+
 try:
 
-    time.sleep(1)
+    time.sleep(.25)
 
-    relay_one.on()
-    time.sleep(.5)
-    relay_two.on()
-
-    time.sleep(2)
-
-    relay_one.off()
-    time.sleep(.5)
-    relay_two.off()
-
-    time.sleep(2)
-
-    relay_one.on()
-    time.sleep(.5)
-    relay_two.on()
-
-    time.sleep(2)
-
-    relay_one.off()
-    time.sleep(.5)
-    relay_two.off()
-
-    time.sleep(1)
+    print("Toggling relay, Zone", zone, "-",state)
+    
     
   
 except RuntimeError as err:
