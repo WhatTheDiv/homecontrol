8c7be4645488c253f72c81147c4f20e764acdfa6
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-07-01 19:00:46 -0400

    update

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -62,10 +62,16 @@ try:
     print("Toggling relay, Zone", zone, "- Requesting new active state:",state)
     print("New active state now?", turnZoneOn(zone, state), flush=True)
     
-    print('readline 1:',sys.stdin.readline(), flush=True)
+    for line in sys.stdin:
+        print('readline 1:',line.rstrip(), flush=True)
+        
     time.sleep(5)
     print('5 ... ', flush=True)
-    print('readline 2:',sys.stdin.readline(), flush=True)
+
+    for line in sys.stdin:
+        print('readline 2:',sys.stdin.readline(), flush=True)
+
+
     while True:
 
         if(len(sys.stdin.readline()) > 0):
