3caede6237c8487738bdc77c93aa96b655f7dc20
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-07-02 18:42:28 -0400

    update

--- server/src/methods/Daemon.js
+++ server/src/methods/Daemon.js
@@ -6,12 +6,15 @@ class DaemonClass {
     this.count = 1
     this.maxCount = 10
     this.checkTimeout_seconds = 2
+    this.log = true
   }
 
   processOutput = (data) => {
     const _d = data.toString()
-    if ('*' === _d.slice(0, 1))
+    if ('*' === _d.slice(0, 1)) {
+      this.log && console.log('(From Daemon)', _d)
       return
+    }
 
     const d = _d.indexOf('\n') >= 0 ? _d.slice(0, _d.indexOf('\n')) : _d
 
