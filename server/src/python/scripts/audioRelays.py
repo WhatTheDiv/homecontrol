394f6e990837d336bfb8285b9975c1deef241a0e
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-07-02 19:04:47 -0400

    update

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -5,70 +5,78 @@ import sys
 import select
 from signal import pause
 
-def turnZoneOn(zone, set_active, z1L, z1R, z2L, z2R):
-  if zone == 1:
-      currState = zone_one_L.is_lit
-      print("Zone 1 currently", currState)
-      if str(currState) == str(set_active):
-          print(" new state matches current, doing nothing ")
-          return currState
-      elif str(set_active) == 'True':
-          print(f'setting zone 1 to True')
-          z1L.on()
-          z1R.on()
-      elif str(set_active) == 'False':
-          print(f'setting zone 1 to False')
-          z1L.off()
-          z1R.off()
-  elif zone == 2:
-      currState = zone_two_L.is_lit
-      print("Zone 2 currently", currState)
-      if str(currState) == str(set_active):
-          print(" new state matches current, doing nothing ")
-          return
-      elif set_active == 'True':
-          z2L.on()
-          z2R.on()
-      elif set_active == 'False':
-          z2L.off()
-          z2R.off()
-  else: 
-      print("out of bounds", zone)
-
-  print(f"Zone {zone} currently lit? {currState}")
-  return set_active
-
-def process_input(input):
-    count = input[:1]
-    command_index = input.find(':') + 1
-    command = input[command_index:command_index + 1]
-    
-    if command == 'a':
-        zone_index = input.find('z') + 1
-        state_index = input.find('-') + 1
-        zone = input[zone_index:zone_index + 1]
-        state = input[state_index:]
-        print(f'* zone:{zone}',flush=True)
-        print(f'* state:{state}',flush=True)
-    
-    print(f'* count:{count}',flush=True)
-    print(f'* command:{command}',flush=True)
-    
-
-  
-
-print("* ")
-print("-------   Starting AudioRelays.py   -------")
-print(" ", flush=True)
-
 try: 
+
     z_1_L = LED(pin=18, initial_value=True)
     z_1_R = LED(pin=19, initial_value=False)
 
     z_2_L = LED(pin=20, initial_value=True)
     z_2_R = LED(pin=21, initial_value=True)
-    print(f'* type:{z_2_R}',flush=True)
-    
+
+    def toggleZone(zone, set_state):
+        if zone === 1:
+            cur_state = z_1_L.is_lit
+            print(f"* Zone 1 currently{curr_state}", flush=True)
+            if curr_state == bool(set_state):
+                print("* New state matches current, do nothing")
+                return True
+            elif bool(set_state):
+                print("* Setting zone 1 to active")
+                z_1_L.on()
+                z_1_r.on()
+                return True
+            else:
+                print("* Setting zone 1 to inactive")
+                z_1_L.off()
+                z_1_r.off()
+                return True
+        elif zone === 2:
+            cur_state = z_2_L.is_lit
+            print(f"* Zone 2 currently{curr_state}", flush=True)
+            if curr_state == bool(set_state):
+                print("* New state matches current, do nothing")
+                return True
+            elif bool(set_state):
+                print("* Setting zone 2 to active")
+                z_2_L.on()
+                z_2_r.on()
+                return True
+            else:
+                print("* Setting zone 2 to inactive")
+                z_2_L.off()
+                z_2_r.off()
+                return True
+        else
+            return False
+
+
+    def process_input(input):
+        count = input[:1]
+        command_index = input.find(':') + 1
+        command = input[command_index:command_index + 1]
+        print(f'* count:{count}',flush=True)
+        print(f'* command:{command}',flush=True)
+
+        if command == 'a':
+            zone_index = input.find('z') + 1
+            state_index = input.find('-') + 1
+            zone = input[zone_index:zone_index + 1]
+            state = input[state_index:]
+            print(f'* zone:{zone}',flush=True)
+            print(f'* state:{state}',flush=True)
+            passed = toggleZone(int(zone), int(state))
+            print(f"{count}:Success:{passed}")
+              
+        
+        
+        
+
+      
+
+    print("* ")
+    print("-------   Starting AudioRelays.py   -------")
+    print(" ", flush=True)
+
 
     while True:
         inp = sys.stdin.readline()
