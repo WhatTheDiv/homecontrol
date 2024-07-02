532cbb329c86d05905c590b1937229c16a2f8a3c
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-07-02 19:21:32 -0400

    update

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -16,33 +16,33 @@ try:
     def toggleZone(zone, set_state):
         if zone == 1:
             curr_state = z_1_L.is_lit
-            print(f"* Zone 1 currently {curr_state}", flush=True)
+            # print(f"* Zone 1 currently {curr_state}", flush=True)
             if curr_state == bool(set_state):
-                print("* New state matches current, do nothing")
+                # print("* New state matches current, do nothing")
                 return True
             elif bool(set_state):
-                print("* Setting zone 1 to active")
+                # print("* Setting zone 1 to active")
                 z_1_L.on()
                 z_1_R.on()
                 return True
             else:
-                print("* Setting zone 1 to inactive")
+                # print("* Setting zone 1 to inactive")
                 z_1_L.off()
                 z_1_R.off()
                 return True
         elif zone == 2:
             curr_state = z_2_L.is_lit
-            print(f"* Zone 2 currently {curr_state}", flush=True)
+            # print(f"* Zone 2 currently {curr_state}", flush=True)
             if curr_state == bool(set_state):
                 print("* New state matches current, do nothing")
                 return True
             elif bool(set_state):
-                print("* Setting zone 2 to active")
+                # print("* Setting zone 2 to active")
                 z_2_L.on()
                 z_2_R.on()
                 return True
             else:
-                print("* Setting zone 2 to inactive")
+                # print("* Setting zone 2 to inactive")
                 z_2_L.off()
                 z_2_R.off()
                 return True
@@ -54,18 +54,18 @@ try:
         count = input[:input.find(':')]
         command_index = input.find(':') + 1
         command = input[command_index:command_index + 1]
-        print(f'* count:{count}',flush=True)
-        print(f'* command:{command}',flush=True)
+        # print(f'* count:{count}',flush=True)
+        # print(f'* command:{command}',flush=True)
 
         if command == 'a':
             zone_index = input.find('z') + 1
             state_index = input.find('-') + 1
             zone = input[zone_index:zone_index + 1]
             state = input[state_index:]
-            print(f'* zone:{zone}',flush=True)
-            print(f'* state:{state}',flush=True)
+            # print(f'* zone:{zone}',flush=True)
+            # print(f'* state:{state}',flush=True)
             passed = toggleZone(int(zone), int(state))
-            print(f"{count}:Success:{passed}", flush=True)
+            return f"{count}:Success:{passed}"
               
         
         
@@ -87,8 +87,8 @@ try:
         elif bool(inp.strip()) == False:
             break
         else:
-            print(f'* {inp}',flush=True)
-            process_input(inp)
+            output = process_input(inp)
+            print(f'{output}',flush=True)
             # TODO pull out command and set relays. print success command
 
 
