dd2417dadafd26c2ca91352a21c461d59ce1b60d
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-07-06 17:56:29 -0400

    update

--- server/src/server.js
+++ server/src/server.js
@@ -84,7 +84,6 @@ app.get('/initialState', async (req, res) => {
     HomeState.audio = a
     HomeState.temp.indoor_temp = temp
     HomeState.temp.indoor_humidity = humidity
-    console.log('#################### t:', HomeState.temp)
   }
   else {
     HomeState.audio.zone_1.updated = false
