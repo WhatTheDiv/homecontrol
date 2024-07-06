722d7206f6a269a56651581a35c9b21cf81a5e9d
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2024-07-06 17:12:29 -0400

    update

--- server/src/python/scripts/aht20.py
+++ server/src/python/scripts/aht20.py
@@ -3,5 +3,5 @@ import adafruit_ahtx0
 
 sensor = adafruit_ahtx0.AHTx0(board.I2C())
 
-print("\nTemperature: %0.1f C" % sensor.temperature)
+print("\nTemperature: %0.1f C" % sensor.temperature * (9 / 5) + 32)
 print("Humidity: %0.1f %%" % sensor.relative_humidity)
