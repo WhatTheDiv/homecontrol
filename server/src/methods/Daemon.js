0cc595f8c21f6767cc52cc84e91f3c8d312f2a3b
Pat <pmcannon93@gmail.com>
2024-08-18 17:56:52 -0400

    updates]

--- server/src/methods/Daemon.js
+++ server/src/methods/Daemon.js
@@ -102,7 +102,7 @@ class DaemonClass {
 
   clearNextOutput = () => {
 
-    if (this.outputs.length > this.maxCount) {
+    if (this.outputs.length >= this.maxCount) {
       console.group('ClearNextOutput')
       console.log('outputs: ', this.outputs)
       console.log('this count: ', this.count)
