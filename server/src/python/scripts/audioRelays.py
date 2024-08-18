2b945ae7beab355bf63bf5085c20b117c35dfb1b
Pat <pmcannon93@gmail.com>
2024-08-18 19:58:12 -0400

    updates]

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -168,7 +168,7 @@ try:
           # return f"{count}:l-{lights_active}/a-{animation_index}"
         
         else:
-           print( f'action: {action.find('l')}', flush=True)
+           print( f'action: {action.find("l")}', flush=True)
            return "success-false"
         
 
