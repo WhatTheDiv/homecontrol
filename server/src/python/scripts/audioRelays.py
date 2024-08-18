77b1def9cff9bb469776fc58bdfc04b71f929cf8
Pat <pmcannon93@gmail.com>
2024-08-18 19:22:10 -0400

    updates]

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -168,7 +168,7 @@ try:
           # return f"{count}:l-{lights_active}/a-{animation_index}"
         
         else:
-           print(f'(python) out of bounds "{input}" - command: {command}, action: {action[:action.find('-')]}', flush=True)
+           print( f'(python) out of bounds "{input}" - command: {command}, action: {action[:action.find("-")]}', flush=True)
            return "success-false"
         
 
