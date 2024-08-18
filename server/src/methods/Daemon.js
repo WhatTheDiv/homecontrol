bff3bd2acb6711e69fd53616d159d51262e2bb10
Pat <pmcannon93@gmail.com>
2024-08-18 17:41:41 -0400

    updates]

--- server/src/methods/Daemon.js
+++ server/src/methods/Daemon.js
@@ -380,6 +380,7 @@ class DaemonClass {
 
   parseReceipt = (name, output) => {
     // -------------------- Break output into sections
+    console.log('output: ', output)
     const trimmedOutput = output.splice(output.indexOf(':') + 1)
     const sections = trimmedOutput.split('/')
 
