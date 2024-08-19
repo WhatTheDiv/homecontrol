15d6e5568b8c8a6327a3eef352ef1e098d793c2e
Pat <pmcannon93@gmail.com>
2024-08-19 14:12:51 -0400

    updates]

---
+++ server/shell/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+clear
+
+echo "updating apt-get"
+
+sudo apt-get update
+sudo apt-get install python3.6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# sudo apt-get install samba -y
+# sudo systemctl enable --now smbd
