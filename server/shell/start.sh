03266e62c79506cf317bf14b906e1f836a2a9363
Pat <pmcannon93@gmail.com>
2024-08-19 14:33:48 -0400

    updates]

--- server/shell/start.sh
+++ server/shell/start.sh
@@ -5,7 +5,7 @@ environtment=$1
 if [[ $environtment && $environtment == '-e' ]]
 then
 echo "Starting up environment ..."
-source src/python/env/bin/activate 
+source server/src/python/env/bin/activate 
 fi
 
 echo "Starting server ... "
