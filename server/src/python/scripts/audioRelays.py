854e8c44ccb58b8db5e112b8001312f14294ce03
Pat <pmcannon93@gmail.com>
2024-08-22 22:16:46 -0400

    updates]

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -33,6 +33,7 @@ try:
               _success = 1  
               _zone1 =  string[string.find('z1-') + 3 : string.find('/')]
               _zone2 =  string[string.find('z2-') + 3: string.find('z2-') + 4]  
+              print(f"zone1: {_zone1}, zone2: {_zone2}", flush=True)
             elif(string.find("fail") >= 0):
               _success = 0
               _err = "fail"     
