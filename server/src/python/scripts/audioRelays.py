2851f50cb8b69e8c01a29ae7e1ee44280715f194
Pat <pmcannon93@gmail.com>
2024-08-22 22:19:06 -0400

    updates]

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -44,7 +44,7 @@ try:
         _success = 0
         _err = "runntimeError"
 
-      print(f"Audio state: z1-{_zone1}, z2-{_zone2}, success-{_success}, err-{_err}", flush=True)
+      # print(f"Audio state: z1-{_zone1}, z2-{_zone2}, success-{_success}, err-{_err}", flush=True)
       return _zone1, _zone2, _success, _err
     def set_Audio(zone=-1, state=-1):
       _success, _err = 0,""
