da7385fe0966c6e2393299b8340012d55d0b6660
Pat <pmcannon93@gmail.com>
2024-08-24 16:15:07 -0400

    updates]

--- server/src/server.js
+++ server/src/server.js
@@ -151,7 +151,7 @@ app.post('/espTouch', async (req, res) => {
       headers: {
         "Content-Type": "text/plain"
       },
-      body: ("Something from the server!\n")
+      body: JSON.stringify("Something from the server!")
     })
 
 
