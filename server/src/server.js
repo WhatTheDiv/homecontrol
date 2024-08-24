167e6e8809f4a957f9c41795e3e915fbb65ef18e
Pat <pmcannon93@gmail.com>
2024-08-24 13:04:56 -0400

    updates]

--- server/src/server.js
+++ server/src/server.js
@@ -136,6 +136,8 @@ app.post('/test', async (req, res) => {
 
 app.get('/espTest', async (req, res) => {
   const { item } = req.body
+
+  res.send(200).end();
 })
 
 app.post('/toggleAudioZones', async (req, res) => {
