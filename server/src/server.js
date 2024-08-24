711e64035a5cd7729d09f540344b6ff8d6dc9878
Pat <pmcannon93@gmail.com>
2024-08-24 15:46:17 -0400

    updates]

--- server/src/server.js
+++ server/src/server.js
@@ -154,7 +154,7 @@ app.post('/espTouch', async (req, res) => {
       body: JSON.stringify("Something from the server!\n\r\r")
     })
 
-    console.log('ard: ', ard)
+    console.log('ard: ', ard.body)
 
   } catch (e) {
     console.error('problem reaching host')
