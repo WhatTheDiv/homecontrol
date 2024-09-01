89ef35311591a4a76d33801ca43dced2eaed3a3c
Pat <pmcannon93@gmail.com>
2024-09-01 02:07:54 -0400

    updates]

--- server/src/methods/wifiModule-methods.js
+++ server/src/methods/wifiModule-methods.js
@@ -173,10 +173,12 @@ class wifiModule {
 
   // returns status { success, fail, error }
   async testCommand(command, sourceId) {
+    console.log('test command: ', { command, sourceId })
     const status = { success: false, fail: false, error: '' }
-    if (command.source === undefined || command.code === undefined)
 
+    if (command.source === undefined || command.code === undefined)
       throw new Error(`malformed request: ${JSON.stringify(command)}`)
+
     if (sourceId < 0)
       throw new Error("Source does not exist")
 
