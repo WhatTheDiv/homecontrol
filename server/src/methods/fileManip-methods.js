86559cb36006f6a3efcdc45468022f582c1b825d
Pat <pmcannon93@gmail.com>
2024-09-01 23:27:41 -0400

    updates]

--- server/src/methods/fileManip-methods.js
+++ server/src/methods/fileManip-methods.js
@@ -5,7 +5,7 @@ async function extractIrCommands() {
   try {
     console.log(process.cwd())
     const res = await fs.readFile('src/StoredData/ircommands.json', { encoding: 'utf-8' })
-    console.log('res: ', res)
+    console.log('res: ', JSON.parse(res))
   } catch (e) {
     console.error(`Failed to extract ir commands: (${e.message})`)
   }
