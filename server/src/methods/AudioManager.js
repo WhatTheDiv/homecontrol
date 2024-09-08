8ad5efd93375f5403f6193ffb03bd2e71cfc8a06
Pat <pmcannon93@gmail.com>
2024-09-07 22:36:49 -0400

    updates]

--- server/src/methods/AudioManager.js
+++ server/src/methods/AudioManager.js
@@ -126,7 +126,7 @@ class audioManager {
   }
 
   async audioSource_changeSource({ IrManager, targetSourceName }) {
-    console.log('checkpoint')
+    console.log('checkpoint', this)
     const status = { success: false, error: false, errorMessage: '' }
     const source = this.audio.sources.find(source => source.name === targetSourceName)
 
