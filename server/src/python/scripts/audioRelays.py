fc18889134a93fead06dc590a18a0efa3057dd8c
Pat <pmcannon93@gmail.com>
2024-09-16 21:34:15 -0400

    updates]

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -157,7 +157,8 @@ try:
         elif command == 'l' and action[:input.find('-')] == 'l': #     Toggle Lights             *****            #--------- 
           try:
             with SMBus(1) as bus:
-              print(f'action: {action[input.find('-') + 1:]}', flush=True)
+              st = action[input.find('-') + 1:]
+              print(f'action: {st}', flush=True)
               t = bytes("lightsToggle/" + action[input.find('-') + 1:], "utf-8")
 
               bus.write_i2c_block_data(lights_slave, 0, t)
