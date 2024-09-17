004a06a805bcd8d37a041d023211dcdcc651ddac
Pat <pmcannon93@gmail.com>
2024-09-16 21:35:56 -0400

    updates]

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -158,11 +158,13 @@ try:
           try:
             with SMBus(1) as bus:
               st = action[input.find('-') + 1:]
-              print(f'* action: {st}', flush=True)
+              
               t = bytes("lightsToggle/" + action[input.find('-') + 1:], "utf-8")
 
               bus.write_i2c_block_data(lights_slave, 0, t)
               block = bus.read_i2c_block_data(lights_slave, 0, 20)
+
+              print(f'* action: {st}', flush=True)
 
               string = ''.join(chr(x) for x in block)
 
