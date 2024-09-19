ae2a9f998e4a7a43d637057fc371aff45321f70e
Pat <pmcannon93@gmail.com>
2024-09-19 15:28:59 -0400

    updates]

--- server/src/methods/Daemon.js
+++ server/src/methods/Daemon.js
@@ -360,6 +360,8 @@ class DaemonClass {
       case 'lights_Toggle': {
         const [l, a] = sections
 
+        console.log(sections)
+
         r.lights.lightsActive = Number(l.slice(l.indexOf('-') + 1)) === 0 ? false : true
         r.lights.animationActive = a.slice(a.indexOf('-') + 1) === 0 ? false : true
         break;
