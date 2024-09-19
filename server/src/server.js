dfeafb75cd9ba4187800032574d9459b072b5bf3
Pat <pmcannon93@gmail.com>
2024-09-19 15:36:45 -0400

    updates]

--- server/src/server.js
+++ server/src/server.js
@@ -453,7 +453,7 @@ app.post('/lights_animation', async (req, res) => {
   const lightsConfig = {
     animationId: !animationNewState ? -1 : getAnimationFromId(animationName)
   }
-  const { err, message, lights } = await Daemon.sendCommand({ name: "lights_setAnimation", lightsConfig, Daemon })
+  const { err, message, lights } = await Daemon.sendCommand({ name: "lights_SetAnimation", lightsConfig, Daemon })
 
   if (err) {
     HomeState.lights.state.updated = false
