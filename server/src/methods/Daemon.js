718bb2c059658dfc0c93b9a7e7d17b4c8075fbff
Pat <pmcannon93@gmail.com>
2024-09-19 15:41:29 -0400

    updates]

--- server/src/methods/Daemon.js
+++ server/src/methods/Daemon.js
@@ -198,7 +198,7 @@ class DaemonClass {
       // -------------------- Initialize variable object
       obj.count = Daemon.count
 
-      console.log('test: ', lightsConfig.animationId === undefined)
+      console.log('test: ', lightsConfig.animationId)
       // -------------------- Configure variable object
       switch (name) {
         case 'audio_State':
