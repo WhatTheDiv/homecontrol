0f5d33a8e714556368af119ae9a347748d3041fd
Pat <pmcannon93@gmail.com>
2024-09-19 13:07:17 -0400

    updates]

--- server/src/python/scripts/audioRelays.py
+++ server/src/python/scripts/audioRelays.py
@@ -158,7 +158,7 @@ try:
               t = bytes("getStyles", "utf-8")
 
               bus.write_i2c_block_data(lights_slave, 0, t)
-              time.sleep(1)
+              time.sleep(2)
               block = bus.read_i2c_block_data(lights_slave, 0, 30)
 
               string = ''.join(chr(x) for x in block)
